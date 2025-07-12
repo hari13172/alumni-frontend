808e40ed0b0dc8d4cbcc7f78d126d12ed41e7af1
hari13172 <haridiablo72@gmail.com>
2025-07-13 02:26:20 +0530

    refactor: Remove QRScanner route from App component

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,13 @@ import AlumniForm from "./pages/protected/Alumini/AluminiForm";
 import AdminLogin from "./pages/protected/Alumini/AdminLogin";
 import AdminRoute from "./pages/protected/Alumini/AdminRoute";
 import AdminPanel from "./pages/protected/Alumini/AdminPanel";
-import QRScanner from "./pages/protected/Alumini/QrScanner";
+
 
 
 const App = () => (
   <Routes>
     {/* Public Route */}
     <Route path="/" element={<Index />} />
-    <Route path="/qr-scanner" element={<QRScanner />} />
 
     {/* User Routes (Assuming these are for alumni, not admin) */}
     <Route path="/alumni/:id" element={<UserProfile />} />
